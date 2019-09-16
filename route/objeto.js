a3298d08cb85179a7bbde2f82734439e5f34a327
Alex <alex2005.uy@gmail.com>
2019-09-16 16:30:42 -0300

    Se deja toda la API funcional

--- route/objeto.js
+++ route/objeto.js
@@ -5,7 +5,7 @@ var Objeto = require("../models/objeto");
 router
     .route("/")
     .get((req,res) => {
-        User.find((err, objeto) => {
+        Objeto.find((err, objeto) => {
             if(err) throw err;
             res.json(objeto);
         })
@@ -28,10 +28,88 @@ router
     });
 
 
+    /** siempre que es u  GET lo ponemos por parametro normalmente */
     router
     .route("/:id")
     .get((req,res) => {
         Objeto.findById(req.params.id,(err, objeto) => {
+            console.log(req.params.id);
+            if(err) throw err;
+            res.json(objeto);
+        })
+    })
+
+     /** siempre actualiza un objeto con id X */
+     router
+     .route("/:id")
+     .put((req,res) => {
+        
+          // const name = req.body.name;
+          const {numeroOrden, tipoObjeto, estadoObjeto, descripcionObjeto, precioBase } = req.body;
+          
+          /* cargo el objeto con el id, quien hace la carga es el Objeto con maysucola que es el elmento del dominio
+          y carga en la variable objeto con minuscula el resultado */
+          Objeto.findById(req.params.id,(err, objeto) => {
+            console.log(req.params.id);
+            if(err) throw err;
+
+            /* actualizamos los datos que vienen en el json */
+            objeto.numeroOrden = numeroOrden;
+            objeto.tipoObjeto = tipoObjeto;
+            objeto.estadoObjeto = estadoObjeto;
+            objeto.descripcionObjeto = descripcionObjeto;
+            objeto.precioBase = precioBase;
+
+            /* al guardar vemos si dio error lo damos si no damos ok */
+            objeto.save(function(err)
+            {
+                if (err)
+                {
+                    res.json(err);
+                } else {
+                    res.json({message: "Objeto se modifico bien)"});
+                }
+            });
+        
+        })
+        
+        
+
+
+     })
+
+
+        /** borrar un objeto con id X */
+        router
+        .route("/:id")
+        .delete((req,res) => {       
+             /* cargo el objeto con el id, quien hace la carga es el Objeto con maysucola que es el elmento del dominio
+             y carga en la variable objeto con minuscula el resultado */
+             Objeto.findById(req.params.id,(err, objeto) => {
+               console.log(req.params.id);
+               if(err) throw err;
+   
+               /* al guardar vemos si dio error lo damos si no damos ok */
+               objeto.delete(function(err)
+               {
+                   if (err)
+                   {
+                       res.json(err);
+                   } else {
+                       res.json({message: "Objeto se elimino bien)"});
+                   }
+              });
+           
+           })
+        })
+     
+
+          /** siempre que es u  GET lo ponemos por parametro normalmente */
+    router
+    .route("/orden/:id")
+    .get((req,res) => {
+        Objeto.find({numeroOrden: req.params.id},(err, objeto) =>      
+        {
             console.log(req.params.id);
             if(err) throw err;
             res.json(objeto);
--- route/tipoObjeto.js
+++ route/tipoObjeto.js
@@ -5,7 +5,7 @@ var TipoObjeto = require("../models/tipo_objeto");
 router
     .route("/")
     .get((req,res) => {
-        User.find((err, tipoObjeto) => {
+        TipoObjeto.find((err, tipoObjeto) => {
             if(err) throw err;
             res.json(tipoObjeto);
         })
@@ -25,4 +25,73 @@ router
 
     });
 
+
+/** siempre que es u  GET lo ponemos por parametro normalmente */
+router
+.route("/:id")
+.get((req,res) => {
+    TipoObjeto.findById(req.params.id,(err, tipoObjeto) => {
+        console.log(req.params.id);
+        if(err) throw err;
+        res.json(tipoObjeto);
+    })
+})
+
+
+
+
+/** siempre actualiza un objeto con id X */
+router
+.route("/:id")
+.put((req,res) => {
+   
+
+     /* cargo el tipoobjeto con el id, quien hace la carga es el TipoObjeto con maysucola que es el elmento del dominio
+     y carga en la variable objeto con minuscula el resultado */
+     TipoObjeto.findById(req.params.id,(err, tipoObjeto) => {
+       console.log(req.params.id);
+       if(err) throw err;
+
+       /* actualizamos los datos que vienen en el json */
+       const { nombreTipo, codigoTipo } = req.body;     
+       tipoObjeto.nombreTipo = nombreTipo;
+       tipoObjeto.codigoTipo = codigoTipo;
+
+       /* al guardar vemos si dio error lo damos si no damos ok */
+       tipoObjeto.save(function(err)
+       {
+           if (err)
+           {
+               res.json(err);
+           } else {
+               res.json({message: "tipo Objeto se modifico bien)"});
+           }
+       });
+   
+   })
+})
+
+   /** borrar un tipo objeto con id X */
+   router
+   .route("/:id")
+   .delete((req,res) => {       
+        /* cargo el tipo objeto con el id, quien hace la carga es el Objeto con maysucola que es el elmento del dominio
+        y carga en la variable objeto con minuscula el resultado */
+        TipoObjeto.findById(req.params.id,(err, tipoobjeto) => {
+          console.log(req.params.id);
+          if(err) throw err;
+
+          /* al guardar vemos si dio error lo damos si no damos ok */
+          tipoobjeto.delete(function(err)
+          {
+              if (err)
+              {
+                  res.json(err);
+              } else {
+                  res.json({message: "Tipo Objeto se elimino bien)"});
+              }
+         });
+      
+      })
+   })
 module.exports = router;
